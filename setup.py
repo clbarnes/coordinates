c5ccaa3ba943a3306099f2f514c7cf9c7530e8d5
Chris Barnes <barnesc@janelia.hhmi.org>
2019-10-16 17:42:29 -0400

    py38

--- setup.py
+++ setup.py
@@ -25,9 +25,10 @@ setup(
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.4',
     ],
     keywords='coordinate spatial mathdict',
     python_requires='>=3.4'
